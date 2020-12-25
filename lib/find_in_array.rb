199f835d00d03acd43879888b2c8fc52237de427
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-25 13:01:43 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -10,10 +10,11 @@
 #   end
 # end
 
-def find_element_index(array, value_to_find)
-  count = 0
-  while count < array.length do
-    puts array[count] == value_to_find
-    count += 1
-  end
-end
+# def find_element_index(array, value_to_find)
+#   count = 0
+#   while count < array.length do
+#     puts array[count] == value_to_find
+#     count += 1
+#   end
+# end
+
